c6a960ec6026e8fdbef566b76958f028dc4caecc
kapilgoel1 <kapilgoel277@gmail.com>
2021-11-16 21:38:13 +0530

    adding ec2 module

--- main.tf
+++ main.tf
@@ -2,6 +2,17 @@ provider "aws" {
     region = "ap-south-1"
 }
 
+module "ec2-instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.0.0"
+  # insert the 4 required variables here
+  ami = "ami-0f1fb91a596abf28d"
+  instance_type = "t2.micro"
+  name = "myterraformserver"
+  vpc_security_group_ids = ["sg-05a791d146988a86a"]
+  subnet_id = "	subnet-077e03f59ce11e817"
+}
+
 
 
 
